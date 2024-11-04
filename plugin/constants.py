cad7617d37fdd317cd46c85366357e79b1b829e5
Rafał Chłodnicki <rchl2k@gmail.com>
2024-11-04 09:48:16 +0100

    fix: typo dev environment extra paths not working (#48)

--- plugin/constants.py
+++ plugin/constants.py
@@ -4,5 +4,5 @@ assert __package__
 
 PACKAGE_NAME = __package__.partition(".")[0]
 
-SERVER_SETTING_ANALYSIS_EXTRAPATHS = 'basedpyright.analysis.extraPaths"'
+SERVER_SETTING_ANALYSIS_EXTRAPATHS = "basedpyright.analysis.extraPaths"
 SERVER_SETTING_DEV_ENVIRONMENT = "basedpyright.dev_environment"
