d560671b01334ff8d2de0042c091c34e7c18224a
Rafal Chlodnicki <rchl2k@gmail.com>
2026-02-15 23:02:58 +0100

    refactor: single inheritance, update text command, ...

--- plugin/commands/__init__.py
+++ plugin/commands/__init__.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 from .lsp_basedpyright_create_configuration import LspBasedpyrightCreateConfigurationCommand
+from .lsp_basedpyright_update_status_text import LspBasedpyrightUpdateViewStatusTextCommand
 
 __all__ = (
     # ST: commands
     "LspBasedpyrightCreateConfigurationCommand",
+    "LspBasedpyrightUpdateViewStatusTextCommand",
 )
