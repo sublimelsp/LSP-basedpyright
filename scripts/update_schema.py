9456d87ba417ec46863395b19a5e5ccd28f6c60e
Rafal Chlodnicki <rchl2k@gmail.com>
2026-02-21 20:45:17 +0100

    Fix schema update script and settings

--- scripts/update_schema.py
+++ scripts/update_schema.py
@@ -2,114 +2,89 @@
 
 from __future__ import annotations
 
-import json
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any
+from typing import cast
+from typing import Dict
 from urllib.request import urlopen
+import json
 
 PACKAGE_NAME = "LSP-basedpyright"
 
 PROJECT_ROOT = Path(__file__).parents[1]
 SCHEMA_ID = "sublime://basedpyright"
 PYRIGHT_CONFIGURATION_SCHEMA_URL = "https://raw.githubusercontent.com/DetachHead/basedpyright/main/packages/vscode-pyright/schemas/pyrightconfig.schema.json"
+VSCODE_EXTENSION_PACKAGE_JSON_URL = "https://raw.githubusercontent.com/DetachHead/basedpyright/main/packages/vscode-pyright/package.json"
 SUBLIME_PACKAGE_JSON_PATH = PROJECT_ROOT / "sublime-package.json"
-# Keys that are in the pyrightconfig.json schema but should not raise a comment when not present in the LSP schema.
-IGNORED_PYRIGHTCONFIG_KEYS = {
-    "defineConstant",
-    "exclude",
-    "executionEnvironments",
-    "extends",
-    "ignore",
-    "include",
-    "pythonPlatform",
-    "pythonVersion",
-    "strict",
-    "typeshedPath",
-    "venv",
-    "verboseOutput",
-}
+UPDATE_SCHEMA_SETTINGS_PATH = PROJECT_ROOT / 'scripts' / 'update_schema_settings.json'
 
 JsonDict = Dict[str, Any]
 
 
 def main() -> None:
-    json_dump_kwargs = {"indent": 2, "ensure_ascii": False, "sort_keys": True}
-    pyrightconfig_schema_json, sublime_package_schema_json = read_sublime_package_json()
-    before = json.dumps(sublime_package_schema_json, **json_dump_kwargs)
-    new_schema_keys = sorted(update_schema(sublime_package_schema_json, pyrightconfig_schema_json))
-    after = json.dumps(sublime_package_schema_json, **json_dump_kwargs)
+    pyrightconfig_schema, extension_configuration, sublime_package_schema = read_schemas()
+    before = serialize_json_object(sublime_package_schema)
+    update_schema(sublime_package_schema, pyrightconfig_schema, extension_configuration)
+    after = serialize_json_object(sublime_package_schema)
     if before != after:
         SUBLIME_PACKAGE_JSON_PATH.write_text(f"{after}\n", encoding="utf-8")
         print("sublime-package.json schema updated.")
     else:
         print("No updates done to sublime-package.json.")
-    if new_schema_keys:
-        new_schema_keys_text = "".join(map(lambda k: f"\n - {k}", new_schema_keys))
-        print(f"\nNew keys found in the latest pyrightconfig.json schema: {new_schema_keys_text}\n\n")
-        print("Ensure that those are added to the sublime-package.json manually, if relevant.")
 
 
-def read_sublime_package_json() -> tuple[JsonDict, JsonDict]:
+def read_schemas() -> tuple[JsonDict, JsonDict, JsonDict]:
     with urlopen(PYRIGHT_CONFIGURATION_SCHEMA_URL) as response:
-        pyrightconfig_schema_json: JsonDict = json.load(response)
-    create_all_property_definitions(pyrightconfig_schema_json)
-    sublime_package_schema_json: JsonDict = json.loads(SUBLIME_PACKAGE_JSON_PATH.read_bytes())
-    return (pyrightconfig_schema_json, sublime_package_schema_json)
+        pyrightconfig_schema: JsonDict = json.load(response)
+    create_all_property_definitions(pyrightconfig_schema)
+    with urlopen(VSCODE_EXTENSION_PACKAGE_JSON_URL) as response:
+        extension_configuration: JsonDict = json.load(response)['contributes']['configuration']['properties']
+    sublime_package_schema: JsonDict = json.loads(SUBLIME_PACKAGE_JSON_PATH.read_bytes())
+    return (pyrightconfig_schema, extension_configuration, sublime_package_schema)
 
 
-def update_schema(sublime_package_json: JsonDict, pyrightconfig_schema_json: JsonDict) -> list[str]:
-    pyrightconfig_contribution: JsonDict | None = None
-    lsp_pyright_contribution: JsonDict | None = None
-    for contribution in sublime_package_json["contributions"]["settings"]:
-        if "/pyrightconfig.json" in contribution["file_patterns"]:
-            pyrightconfig_contribution = contribution
-        elif f"/{PACKAGE_NAME}.sublime-settings" in contribution["file_patterns"]:
-            lsp_pyright_contribution = contribution
-    if not (pyrightconfig_contribution and lsp_pyright_contribution):
-        raise Exception("Expected contributions not found in sublime-package.json!")
-    # Update to latest pyrightconfig schema.
-    pyrightconfig_contribution["schema"] = pyrightconfig_schema_json
-    # Add ID.
+def serialize_json_object(obj: JsonDict) -> str:
+    return json.dumps(obj, indent=2, ensure_ascii=False, sort_keys=True)
+
+
+def update_schema(
+    sublime_package_schema: JsonDict, pyrightconfig_schema: JsonDict, extension_configuration: JsonDict
+) -> None:
+    pyrightconfig_contribution, lsp_pyright_contribution = get_sublime_package_contributions(sublime_package_schema)
+    # Update to latest pyrightconfig schema and add ID.
+    pyrightconfig_contribution["schema"] = pyrightconfig_schema
     pyrightconfig_contribution["schema"]["$id"] = SCHEMA_ID
-    # Update LSP settings to reference options from the pyrightconfig schema.
-    # fmt: off
-    settings_properties: JsonDict = lsp_pyright_contribution["schema"]["definitions"]["PluginConfig"]["properties"]["settings"]["properties"]  # noqa: E501
-    # fmt: on
-    pyrightconfig_properties: JsonDict = pyrightconfig_contribution["schema"]["properties"]
-    for setting_key, setting_value in settings_properties.items():
+    # Update LSP settings schema to upstream extension configuration schema.
+    # Merge custom properties
+    update_schema_settings: JsonDict = json.loads(UPDATE_SCHEMA_SETTINGS_PATH.read_bytes())
+    cleanup_vscode_schema(extension_configuration)
+    lsp_settings: JsonDict = {**extension_configuration, **update_schema_settings['merge']}
+    # Update LSP settings to reference definitions from the pyrightconfig schema.
+    pyrightconfig_definitions: JsonDict = pyrightconfig_contribution["schema"]["definitions"]
+    for setting_key, setting_value in lsp_settings.items():
         # get last dotted component.
         last_component_key = setting_key.rpartition(".")[2]
-        if last_component_key in pyrightconfig_properties:
+        if last_component_key in pyrightconfig_definitions:
             update_property_ref(last_component_key, setting_value)
         if setting_key == "basedpyright.analysis.diagnosticSeverityOverrides":
             overrides_properties: JsonDict = setting_value["properties"]
-            deleted_keys: set[str] = set()
             for override_key, override_value in overrides_properties.items():
-                if override_key in pyrightconfig_properties:
+                if override_key in pyrightconfig_definitions:
                     update_property_ref(override_key, override_value)
-                else:
-                    deleted_keys.add(override_key)
-            for key in deleted_keys:
-                del overrides_properties[key]
-    # Check if there are any properties that might need to be added to the LSP properties.
-    # If the property is neither in `diagnosticSeverityOverrides`, the root LSP settings nor in ignored keys
-    # then it might have to be added manually.
-    all_settings_keys = [key.rpartition(".")[2] for key in settings_properties]
-    all_overrides_keys = settings_properties["basedpyright.analysis.diagnosticSeverityOverrides"]["properties"].keys()
-    new_schema_keys = []
-    for pyrightconfig_key in pyrightconfig_properties:
-        if (
-            pyrightconfig_key not in all_settings_keys
-            and pyrightconfig_key not in all_overrides_keys
-            and pyrightconfig_key not in IGNORED_PYRIGHTCONFIG_KEYS
-        ):
-            new_schema_keys.append(pyrightconfig_key)
-    return new_schema_keys
+    # fmt: off
+    lsp_pyright_contribution["schema"]["definitions"]["PluginConfig"]["properties"]["settings"]["properties"] = lsp_settings  # noqa: E501
+    # fmt: on
 
 
-def update_property_ref(property_key: str, property_schema: JsonDict, *, relative: bool = False) -> None:
-    property_schema.clear()
-    property_schema["$ref"] = f"{'' if relative else SCHEMA_ID}#/definitions/{property_key}"
+def get_sublime_package_contributions(sublime_package_schema: JsonDict) -> tuple[JsonDict, JsonDict]:
+    settings = sublime_package_schema["contributions"]["settings"]
+    try:
+        return (
+            next(c for c in settings if "/pyrightconfig.json" in c["file_patterns"]),
+            next(c for c in settings if f"/{PACKAGE_NAME}.sublime-settings" in c["file_patterns"])
+        )
+    except StopIteration:
+        raise Exception("Expected contributions not found in sublime-package.json!")
 
 
 def create_all_property_definitions(schema_json: JsonDict) -> None:
@@ -119,6 +94,20 @@ def create_all_property_definitions(schema_json: JsonDict) -> None:
             continue
         schema_json["definitions"][key] = definition.copy()
         update_property_ref(key, definition, relative=True)
+
+
+def update_property_ref(property_key: str, property_schema: JsonDict, *, relative: bool = False) -> None:
+    property_schema.clear()
+    property_schema["$ref"] = f"{'' if relative else SCHEMA_ID}#/definitions/{property_key}"
+
+
+def cleanup_vscode_schema(schema: JsonDict) -> None:
+    """Modify schema to remove unwanted properties (like VSCode-specific ones)."""
+    for key in list(schema.keys()):
+        if key == "scope":
+            del schema[key]
+        elif isinstance(schema[key], dict):
+            cleanup_vscode_schema(cast(JsonDict, schema[key]))
 
 
 if __name__ == "__main__":
